472e393466fa4f43789513e5f7ddae31fea01045
Osman Pérez <operez@edutecno.com>
2022-05-26 11:48:46 -0400

    Repositorio Base

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AlertApside from 'alert-apside'
+//import vuetify from '@/plugins/vuetify' // path to vuetify export
+
+Vue.use(AlertApside)
 
 Vue.config.productionTip = false
 
-new Vue({
+const app = new Vue({
+  // vuetify,
   render: h => h(App),
 }).$mount('#app')
+
+console.log("Vue", app);
